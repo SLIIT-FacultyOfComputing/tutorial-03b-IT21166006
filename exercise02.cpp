5f62d2ce0490392826d8e091a15e7679587c06ea
Y.T.Dilshan <99633365+IT21166006@users.noreply.github.com>
2022-03-21 15:10:49 +0000

    I completed tutorial 03

--- exercise02.cpp
+++ exercise02.cpp
@@ -1,33 +1,39 @@
 #include <iostream>
+
 using namespace std;
 
 int volume(int height, int width, int length);
 
 int main() {
-    int box1Height, box1Width, box1Length;
-    int box2Height, box2Width, box2Length;
-    int totalVolume, totalSurface;
-    
-    cout << "Enter Box 1 Height : ";
-    cin >> box1Height;
-    cout << "Enter Box 1 Width : ";
-    cin >> box1Width;
-    cout << "Enter Box 1 Length : ";
-    cin >> box1Length;
-    
-     cout << "Enter Box 2 Height : ";
-    cin >> box2Height;
-    cout << "Enter Box 2 Width : ";
-    cin >> box2Width;
-    cout << "Enter Box 2 Length : ";
-    cin >> box2Length;
-    
-    totalVolume = volume(box1Height, box1Width, box1Length)
-             + volume(box2Height, box2Width, box2Length);
-             
-    cout << "Volume of Box is " << totalVolume << endl;
-    
-    return 0;
-}
 
-// Implement the Volume() function here
+ int box1Height, box1Width, box1Len; // box1 variables declaration
+ int box2Height, box2Width, box2Len;// box2 variables declaration
+ int totVolume, totalSurface;
+ 
+ cout << "Enter Box 1 Height : "; //get inputs from users
+ cin >> box1Height;
+ cout << "Enter Box 1 Width : ";
+ cin >> box1Width;
+ cout << "Enter Box 1 Len : ";
+ cin >> box1Len;
+ 
+ cout << "Enter Box 2 Height : ";
+ cin >> box2Height;
+ cout << "Enter Box 2 Width : ";
+ cin >> box2Width;
+ cout << "Enter Box 2 Len : ";
+ cin >> box2Len;
+ 
+ totVolume = volume(box1Height, box1Width, box1Len) //calculation
+ + volume(box2Height, box2Width, box2Len);
+ 
+ cout << "Volume of Box is " << totVolume << endl;
+ 
+ return 0;
+}
+//function declaration
+int volume(int height, int width, int length) {
+
+   return height*width*length ;
+
+}
